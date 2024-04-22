49581d8955048588ddf2a4c932de8920002208af
CameronAllenby <146819191+CameronAllenby@users.noreply.github.com>
2024-04-22 16:09:12 +0100

    rtg
    er

--- Assets/Script/Player Controles.cs
+++ Assets/Script/Player Controles.cs
@@ -5,6 +5,16 @@ using Cinemachine;
 using UnityEngine.InputSystem;
 using UnityEngine.InputSystem.XR;
 
+public enum States
+{
+    None,
+    Idle,
+    Walk,
+    Jump,
+    Death,
+    Glide,
+    Aim,
+}
 public class PlayerControles : MonoBehaviour
 {
     [SerializeField] private Rigidbody rb;
@@ -13,6 +23,85 @@ public class PlayerControles : MonoBehaviour
     private Vector2 cameraInputValue;
     private float yRot;
     public float mouseSensitivity = 1f;
+    private Animator anim;
+
+    States state;
+
+    bool grounded;
+
+    private void Start()
+    {
+        anim = GetComponent<Animator>();
+        rb = GetComponent<Rigidbody>();
+        state = States.Idle;
+    }
+    void DoLogic()
+    {
+        if (state == States.Idle)
+        {
+            PlayerIdle();
+        }
+
+        if (state == States.Jump)
+        {
+            PlayerJumping();
+        }
+
+        if (state == States.Walk)
+        {
+            PlayerWalk();
+        }
+
+        if (state == States.Death)
+        {
+            PlayerDeath();
+        }
+    }
+
+    void PlayerIdle()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            // simulate jump
+            state = States.Jump;
+            rb.velocity = new Vector3(0, 100, 0);
+        }
+
+        anim.SetBool("walk", false);
+        if (Input.GetAxis("Mouse X") != 0f)
+        {
+            state = States.Walk;
+        }
+    }
+
+    void PlayerJumping()
+    {
+        // player is jumping, check for hitting the ground
+        if (grounded == true)
+        {
+            //player has landed on floor
+            state = States.Idle;
+        }
+    }
+
+    void PlayerWalk()
+    {
+        transform.Translate(moveInputValue.x * speed * Time.fixedDeltaTime, 0, moveInputValue.y * speed * Time.fixedDeltaTime);
+        yRot += Input.GetAxis("Mouse X") * mouseSensitivity;
+        if (rb.velocity.z != 0)
+        {
+            transform.localEulerAngles = new Vector3(transform.localEulerAngles.x, yRot, transform.localEulerAngles.z);
+        }
+        if (moveInputValue.x == 0f && moveInputValue.y == 0f)
+        {
+            state = States.Idle;
+        }
+    }
+
+    void PlayerDeath()
+    {
+
+    }
 
     private void OnNewaction(InputValue value)
     {
@@ -24,18 +113,27 @@ public class PlayerControles : MonoBehaviour
         cameraInputValue = value.Get<Vector2>();
         Debug.Log(cameraInputValue);
     }
-    private void MovementLogic()
+    void OnCollisionEnter(Collision col)
     {
-        transform.Translate(moveInputValue.x * speed * Time.fixedDeltaTime, 0, moveInputValue.y * speed * Time.fixedDeltaTime);
-        yRot += Input.GetAxis("Mouse X") * mouseSensitivity;
-        if (rb.velocity.z != 0)
+        if (col.gameObject.tag == "Floor")
         {
-            transform.localEulerAngles = new Vector3(transform.localEulerAngles.x, yRot, transform.localEulerAngles.z);
+            grounded = true;
+            print("landed!");
+        }
+
+        if (col.gameObject.tag == "EvilCubeTheDestroyerOfWorlds")
+        {
+            state = States.Death;
         }
     }
-
-    private void FixedUpdate()
+    private void OnGUI()
     {
-        MovementLogic();
+        //debug text
+        string text = "Left/Right arrows = Rotate\nSpace = Jump\nUp Arrow = Forward\nCurrent state=" + state;
+
+        // define debug text area
+        GUILayout.BeginArea(new Rect(10f, 450f, 1600f, 1600f));
+        GUILayout.Label($"<size=16>{text}</size>");
+        GUILayout.EndArea();
     }
 }
